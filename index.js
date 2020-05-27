2508896166d8047e13796b206ecfb93e9deb94c3
Tree69420 <65974506+Tree69420@users.noreply.github.com>
2020-05-27 15:07:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var io = require('socket.io')(server);
 
 
 var gameNumber = 1;
-var gameList = {};
+var games = [];
 app.get('/login', function (req, res) {
   res.sendFile(__dirname+"/client/index.html");
 })
@@ -26,15 +26,14 @@ function addGames(socket){
 io.on('connection', function(socket) {
   socket.join("menu");
 
-  io.emit('addGames', gameList);
+  io.emit('addGames', games);
 
 
 
   socket.on('newGame', function(msg) {
 
-    gameList[gameNumber+""]={name:msg};
-    console.log(gameList);
-    io.emit('addGames', gameList);
+    games.push(gameNumber);
+    io.emit('addGames', games);
     socket.emit('joinedGame',0);
     socket.leave("menu");
     socket.join(gameNumber);
@@ -43,18 +42,29 @@ io.on('connection', function(socket) {
   });
   socket.on('message', function(msg) {
     var rooms = Object.keys(socket.rooms).filter(item => item!=socket.id);
+    console.log(rooms[0]);
     io.to(rooms[0]).emit('message', msg);
     
   });
   socket.on('joinGame', function(msg) {
+    console.log(msg);
     socket.emit('joinedGame',0);
     socket.leave("menu");
     socket.join(msg);
   });
   socket.on('leaveRoom',function(msg){
     var rooms = Object.keys(socket.rooms).filter(item => item!=socket.id);
+    console.log(rooms[0]);
     socket.leave(rooms[0]);
     socket.join("menu");
     socket.emit('leaveRoom',0);
+    /*
+      if (roommembers == 0){
+        deletegame();
+      }
+    */
   });
 });
+
+
+
